dd9468e8e4dbc0da651a26775617ee2adbdd58c2
Nicolas Burghi <nicoburghi@gmail.com>
2020-10-28 21:47:39 +0100

    add react scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types"
 import React from "react"
 import { useInView } from 'react-intersection-observer';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 const Navbar = ({ items }) => {
     const [ref, inView] = useInView({
@@ -12,10 +13,60 @@ const Navbar = ({ items }) => {
         <div ref={ref}>
             <nav id="nav" className={!inView ? 'alt' : ''}>
                 <ul>
+                    <li>
+                        <Link
+                                activeClass="active"
+                                to="intro"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Intro
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                                activeClass="active"
+                                to="english-lessons"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                English Lessons
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                                activeClass="active"
+                                to="spanish-lessons"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Spanish Lessons
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration={500}
+                            >
+                                Sobre mi
+                        </Link>
+                    </li>
+                    {/*
                     <li><a href="#intro" className="active">Intro</a></li>
                     <li><a href="#english-lessons">English Lessons</a></li>
                     <li><a href="#spanish-lessons">Spanish Lessons</a></li>
                     <li><a href="#about">Sobre mi</a></li>
+                    */}
                 </ul>
             </nav>
         </div>
